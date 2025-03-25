e89fdcef87e1c052e0d8e60ac45791a01a9aba3d
Nimesha Abeyrathna <134591165+Nimesha07@users.noreply.github.com>
2025-03-26 00:00:07 +0530

    init

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Travel Management System</h1>
+      <p>Plan your trips with ease!</p>
+    </div>
+  );
+};
+
+export default Home;
